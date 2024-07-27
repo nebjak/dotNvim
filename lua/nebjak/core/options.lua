76e0c17e692e415841b5f93f8e91924ed9ddb22a
Nebojsa Jakovljevic <nebojsa@nebjak.net>
2024-07-27 23:40:06 +0200

    Use tree list style for default file explorer

--- lua/nebjak/core/options.lua
+++ lua/nebjak/core/options.lua
@@ -1,3 +1,5 @@
+vim.cmd("let g:netrw_liststyle = 3")
+
 -- Search settings
 vim.opt.ignorecase = true -- case insensitive
 vim.opt.smartcase = true -- be smart about case
@@ -9,7 +11,7 @@ vim.opt.magic = true
 
 -- Enable spell check by default
 vim.opt.spell = true
-vim.opt.spelllang = 'en_us'
+vim.opt.spelllang = "en_us"
 
 -- Tabs & indentation
 vim.opt.shiftwidth = 2 -- 2 spaces for indent width
@@ -18,12 +20,11 @@ vim.opt.expandtab = true -- expand tabs to spaces
 vim.opt.autoindent = true -- copy indent from current line when starting new one
 
 -- Show list
-vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fileencoding = "utf-8"
 vim.opt.list = true
-vim.opt.listchars = { tab = '▸ ', eol = '¬' }
-
+vim.opt.listchars = { tab = "▸ ", eol = "¬" }
 
 -- Set relative number lines
 vim.opt.relativenumber = true
@@ -32,15 +33,15 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 -- Disable arrow keys
-vim.api.nvim_set_keymap('', '<up>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('', '<down>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('', '<left>', '<nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('', '<right>', '<nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<up>", "<nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<down>", "<nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<left>", "<nop>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("", "<right>", "<nop>", { noremap = true, silent = true })
 
 -- copy
-vim.api.nvim_set_keymap('v', '<leader>y', '"*y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>y", '"*y', { noremap = true, silent = true })
 -- paste
-vim.api.nvim_set_keymap('v', '<leader>p', '"*p', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "<leader>p", '"*p', { noremap = true, silent = true })
 
 -- Colors
 vim.opt.termguicolors = true
